36f1284e3174ca460c1f2263c749c0cbe655c115
Darren Bowers <darren.bowers@g.austincc.edu>
2021-12-01 14:29:23 -0600

    updated to create two seperate objects to test both constructors

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ Purpose: Program to recreate string class similar to what is found in Python or 
 
 
 int main(){
-    cout << "Creating a string of 'Hello World'" << endl;
-    MyString mystring("Hello World");
-    cout << "The length of the string is " << mystring.getLength() << endl;
+    cout << "Creating myString1 with 'Hello World'" << endl;
+    MyString myString1("Hello World");
+    cout << "The length of the string is " << myString1.getLength() << endl << endl;
 
+    cout << "Creating myString2 an empty string" << endl;
+    MyString myString2;
+    cout << "The length of the string is " << myString2.getLength() << endl;
 
 
 
