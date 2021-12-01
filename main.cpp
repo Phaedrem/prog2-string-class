8d69b94856c21d9e124c0d42d4f0873825e24915
Darren Bowers <darren.bowers@g.austincc.edu>
2021-12-01 15:38:03 -0600

    added calls to test set method and report length of new strings

--- main.cpp
+++ main.cpp
@@ -14,11 +14,16 @@ int main(){
 
     cout << "Creating myString2 an empty string" << endl;
     MyString myString2;
-    cout << "The length of the string is " << myString2.getLength() << endl;
+    cout << "The length of the string is " << myString2.getLength() << endl << endl;
+    
+    
+    cout << "Emptying myString1" << endl; 
+    myString1.set();
+    cout << "The length of the tring in myString1 is now " << myString1.getLength() << endl << endl;
 
-
-
-
+    cout << "Placing 'Otters Rock Socks' into myString2" << endl;
+    myString2.set("Otters Rock Socks");
+    cout << "The length of the string myString2 is now " << myString2.getLength() << endl << endl;
 
 
 
