7f83e7a27a4f8c16e69f0001608f3b3f0b358e8e
Darren Bowers <darren.bowers@g.austincc.edu>
2021-12-05 15:36:54 -0600

    created swap method to be used later in substring

--- mystring.cpp
+++ mystring.cpp
@@ -31,11 +31,11 @@ MyString::~MyString(){
 PRIVATE
 *********************************/
 
-
-
-
-
-
+void MyString::swap(int *number1, int *number2){
+    int temp = *number1;
+    *number1 = *number2;
+    *number2 = temp;
+}
 
 
 /*********************************
--- mystring.h
+++ mystring.h
@@ -24,6 +24,7 @@ public:
 private:
     int length;
     char *array;
+    void swap(int*, int*);
 
 };
 
