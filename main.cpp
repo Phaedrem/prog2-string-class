61231fe0fed037455112e732576ed95ba0c06c8f
Darren Bowers <darren.bowers@g.austincc.edu>
2021-12-05 20:33:22 -0600

    added some couts fixced some grammar to provide a cleaner presentation. Also called several substrings for testing

--- main.cpp
+++ main.cpp
@@ -8,44 +8,109 @@ Purpose: Program to recreate string class similar to what is found in Python or 
 
 
 int main(){
+    cout << "~~~~~~~~~~~~ Constructors ~~~~~~~~~~~~~~" << endl << endl;
+
     cout << "Creating myString1 with \"Hello World\"" << endl;
     MyString myString1("Hello World");
-    cout << "The length of the string is " << myString1.getLength() << endl << endl;
+    cout << "The length of the string is " << myString1.getLength() << endl; 
+    cout << "myString1 contains \"" << myString1.getString() << "\""<< endl << endl; 
 
     cout << "Creating myString2 as an empty string" << endl;
     MyString myString2;
-    cout << "The length of the string is " << myString2.getLength() << endl << endl;
+    cout << "The length of the string is " << myString2.getLength() << endl;
+    cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl;
     
-    
+    cout << "~~~~~~~~~~~~~~ Clear and Set ~~~~~~~~~~~~~~" << endl << endl; 
+ 
     cout << "Emptying myString1" << endl; 
     myString1.clear();
     cout << "The length of the string in myString1 is now " << myString1.getLength() << endl << endl;
 
-    cout << "Placing \"Otters Rock Socks\" into myString2" << endl;
-    myString2.set("Otters Rock Socks");
+    cout << "Placing \"Hello World\" into myString2" << endl;
+    myString2.set("Hello World");
     cout << "The length of the string myString2 is now " << myString2.getLength() << endl << endl;
 
     cout << "myString1 contains \"" << myString1.getString() << "\""<< endl; 
     cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl; 
 
+    cout << "~~~~~~~~~~~~~~ Reverse ~~~~~~~~~~~~~~" << endl << endl; 
+
     cout << "Reversing myString2 WITHOUT saving it " << endl; 
     cout << myString2.reverse() << endl << endl; 
 
-    cout << "using getString to show that the original array has NOT changed" << endl; 
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
     cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl; 
 
     cout << "Reversing myString2 AND saving it " << endl; 
     cout << myString2.reverse(true) << endl << endl;
 
-    cout << "using getString to show that the original array HAS changed" << endl; 
+    cout << "Using getString to show that the original array HAS changed" << endl; 
     cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl; 
+
+    cout << "Putting it back " << endl; 
+    cout << myString2.reverse(true) << endl << endl;
 
     cout << "Reversing myString1 WITHOUT saving it " << endl; 
     cout << myString1.reverse() << endl << endl; 
 
-    cout << "using getString to show that the original array has NOT changed" << endl; 
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
     cout << "myString1 contains \"" << myString1.getString() << "\""<< endl << endl;
 
+    cout << "~~~~~~~~~~~~~~ Substring ~~~~~~~~~~~~~~" << endl << endl; 
+
+    cout << "Testing myString2.substringe - just one letter, both IN range, smaller number first, not saving changes" << endl;
+    try{
+        cout << myString2.substring(0,0) << endl;    
+    } catch(...){
+        cout << "Error, requested index[s] out of range" << endl;
+    }
+
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
+    cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl; 
+
+    cout << "Testing myString2.substringe - 3 letters, IN range, smaller number first, not saving changes" << endl;
+    try{
+        cout << myString2.substring(1,3) << endl;    
+    } catch(...){
+        cout << "Error, requested index[s] out of range" << endl;
+    }
+
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
+    cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl;
+
+    cout << "Testing myString2.substringe - 3 letters, IN range, larger number first, not saving changes" << endl;
+    try{
+        cout << myString2.substring(3,1) << endl;    
+    } catch(...){
+        cout << "Error, requested index[s] out of range" << endl;
+    }
+
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
+    cout << "myString2 contains \"" << myString2.getString() << "\""<< endl << endl;
+
+    cout << "Testing empty myString1.substringe" << endl;
+    try{
+        cout << myString1.substring(3,1) << endl;    
+    } catch(int a){
+        cout << "Error, requested index[s] out of range" << endl;
+    } catch(...){
+        cout << "Error, string is empty" << endl; 
+    }
+
+    cout << "Using getString to show that the original array has NOT changed" << endl; 
+    cout << "myString1 contains \"" << myString1.getString() << "\""<< endl << endl;
+
+
+    cout << "Testing myString2.substringe - 3 letters, IN range, larger number first, SAVING changes" << endl;
+    try{
+        cout << myString2.substring(3,1,true) << endl;    
+    } catch(...){
+        cout << "Error, requested index[s] out of range" << endl;
+    }
+
+    cout << "Using getString to show that the original array HAS changed" << endl; 
+    cout << "myString2 contains \"" << myString2.getString() << "\""<< endl;
+    cout << "The length of the string is " << myString2.getLength() << endl << endl;
 
 
 
